19cbbb9fb3da6c7a1e06cfbbb08c48edff2427d3
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2020-02-02 14:24:33 +0900

    add forAll

--- src/main/scala/testing/Gen.scala
+++ src/main/scala/testing/Gen.scala
@@ -3,7 +3,7 @@ package testing
 import state.State
 import state.RNG
 
-trait Prop {
+case class Prop(run: (Prop.TestCases,RNG) => Result) {
     // def check: Either[(FailedCase, SuccessCount), SuccessCount]
 
     // def &&(p: Prop): Prop = {
@@ -12,8 +12,23 @@ trait Prop {
 }
 
 object Prop {
+    type TestCases = Int
+    type Result = Option[(FailedCase,SuccessCount)]
     type SuccessCount = Int
     type FailedCase = String
+}
+
+sealed trait Result {
+    def isFalsified: Boolean
+}
+
+case object Passed extends Result {
+    def isFalsified: Boolean = false
+}
+
+case class Falsified(failure: Prop.FailedCase,
+                     successes: Prop.SuccessCount) extends Result {
+    def isFalsified: Boolean = true
 }
 
 case class Gen[+A](sample: State[RNG,A]) {
@@ -44,8 +59,21 @@ object Gen {
         Gen(State(RNG.double).flatMap(rng => if (rng > g1Prob) g1._1.sample else g2._1.sample))
     }
 
-    def forAll[A](a: Gen[A])(f: A => Boolean): Prop =
-        return null
+    def forAll[A](a: Gen[A])(f: A => Boolean): Prop = Prop {
+        (n,rng) => randomStream(a)(rng).zip(Stream.from(0)).take(n).map {
+            case (a, i) => try {
+                if (f(a)) Passed else Falsified(a.toString, i)
+            } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+        }.find(_.isFalsified).getOrElse(Passed)
+    }
+
+    def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+        Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+    def buildMsg[A](s: A, e: Exception): String =
+        s"test case: $s\n" +
+        s"generated an exception: ${e.getMessage}\n" +
+        s"stack trace:\n ${e.getStackTrace.mkString("\n")}"        
 
     def choose(start: Int, stopExclusive: Int): Gen[Int] = {
         Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
