2f8fee86a4594013cf1b07d27c9296fa84f51616
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2020-02-15 09:53:24 +0900

    implement monad trait

--- src/main/scala/monad/Monad.scala
+++ src/main/scala/monad/Monad.scala
@@ -1,11 +1,32 @@
 package monad
 
+import testing.Gen
+
 trait Functor[F[_]] {
-  def map[A, B](fa: F[A])(f: A => B): F[B]
-  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+  def map[A,B](fa: F[A])(f: A => B): F[B]
+  def distribute[A,B](fab: F[(A, B)]): (F[A], F[B]) =
     (map(fab)(_._1), map(fab)(_._2))
-  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
     case Left(value)  => map(value)(Left(_))
     case Right(value) => map(value)(Right(_))
+  }
+}
+
+trait Monad[F[_]] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A,B](fa: F[A])(f: A => F[B]): F[B]
+
+  def map[A,B](fa: F[A])(f: A => B): F[B] =
+    flatMap(fa)(a => unit(f(a)))
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a,b)))
+}
+
+object Monad {
+
+  def genMonad = new Monad[Gen] {
+    def unit[A](a: => A): Gen[A] = Gen.unit(a)
+    def flatMap[A, B](fa: Gen[A])(f: A => Gen[B]): Gen[B] =
+      fa.flatMap(f)
   }
 }
