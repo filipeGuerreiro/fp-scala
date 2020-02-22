424a9452491924f2dfc535e47680cf531793005f
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2020-02-22 14:51:48 +0900

    implement the reader monad

--- src/main/scala/monad/Monad.scala
+++ src/main/scala/monad/Monad.scala
@@ -93,3 +93,13 @@ case class Id[A](value: A) {
   def map[B](f: A => B): Id[B] = Id(f(value))
   def flatMap[B](f: A => Id[B]): Id[B] = f(value)
 }
+
+case class Reader[R,A](run: R => A)
+
+object Reader {
+  def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
+    def unit[A](a: => A): Reader[R,A] = Reader(r => a)
+    def flatMap[A, B](fa: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] =
+      Reader(r => f(fa.run(r)).run(r))
+  }
+}
