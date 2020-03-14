2558713306c60bc7c6df769500d328c9dd539502
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2020-03-14 21:11:00 +0900

    define apply

--- src/main/scala/applicative/Applicative.scala
+++ src/main/scala/applicative/Applicative.scala
@@ -3,11 +3,15 @@ package applicative
 import monad.Functor
 
 trait Applicative[F[_]] extends Functor[F] {
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)((f, a) => f(a))
   def unit[A](a: => A): F[A]
 
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(map(fa)(f.curried))(fb)
+
   def map[A, B](fa: F[A])(f: A => B): F[B] =
-    map2(fa, unit(()))((a, _) => f(a))
+    apply(unit(f))(fa)
 
   def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
