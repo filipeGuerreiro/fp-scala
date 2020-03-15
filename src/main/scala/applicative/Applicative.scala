b42f2b219c3db6fc4ad96b2e57150311ce80f1ff
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2020-03-15 09:20:04 +0900

    implement map3 and 4 in terms of apply and map

--- src/main/scala/applicative/Applicative.scala
+++ src/main/scala/applicative/Applicative.scala
@@ -22,4 +22,10 @@ trait Applicative[F[_]] extends Functor[F] {
     sequence(List.fill(n)(fa))
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = 
     map2(fa, fb)((a, b) => (a, b))
+
+  def map3[A,B,C,D](fa: F[A], fb: F[B], fc: F[C])(f: (A,B,C) => D): F[D] =
+    apply(apply(map(fa)(f.curried))(fb))(fc)
+
+  def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A,B,C,D) => E): F[E] =
+    apply(apply(apply(map(fa)(f.curried))(fb))(fc))(fd)
 }
