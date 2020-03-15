7b4bed0a8f58fff71afebc81de9e3b04d1ef9489
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2020-03-15 09:20:48 +0900

    remove empty line

--- src/main/scala/applicative/Applicative.scala
+++ src/main/scala/applicative/Applicative.scala
@@ -25,7 +25,6 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def map3[A,B,C,D](fa: F[A], fb: F[B], fc: F[C])(f: (A,B,C) => D): F[D] =
     apply(apply(map(fa)(f.curried))(fb))(fc)
-
   def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A,B,C,D) => E): F[E] =
     apply(apply(apply(map(fa)(f.curried))(fb))(fc))(fd)
 }
