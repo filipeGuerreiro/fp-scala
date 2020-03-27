b44531e070cd9a90adf75d3cfab88a2f6b632ced
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2020-03-27 19:56:01 +0900

    local effects classes

---
+++ src/main/scala/mutable/STMonad.scala
@@ -0,0 +1,51 @@
+package mutable
+
+sealed trait ST[S,A] { self =>
+  protected def run(s: S): (A,S)
+  def map[B](f: A => B): ST[S,B] = new ST[S,B] {
+    def run(s: S) = {
+      val (a, s1) = self.run(s)
+      (f(a), s1)
+    }
+  }
+  def flatMap[B](f: A => ST[S,B]): ST[S,B] = new ST[S,B] {
+    def run(s: S) = {
+      val (a, s1) = self.run(s)
+      f(a).run(s1)
+    }
+  }
+}
+
+object ST {
+  def apply[S,A](a: => A) = {
+    lazy val memo = a
+    new ST[S,A] {
+      def run(s: S) = (memo, s)
+    }
+  }
+  def runST[A](st: RunnableST[A]): A =
+    st[Unit].run(())._1
+}
+
+trait RunnableST[A] {
+  def apply[S]: ST[S,A]
+}
+
+sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
+  protected def value: Array[A]
+  def size: ST[S,Int] = ST(value.size)
+
+  // Write a value at the give index of the array
+  def write(i: Int, a: A): ST[S,Unit] = new ST[S,Unit] {
+    def run(s: S) = {
+      value(i) = a
+      ((), s)
+    }
+  }
+
+  // Read the value at the given index of the array
+  def read(i: Int): ST[S,A] = ST(value(i))
+
+  // Turn the array into an immutable list
+  def freeze: ST[S,List[A]] = ST(value.toList)
+}
