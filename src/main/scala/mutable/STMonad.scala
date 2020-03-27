4d74e81c776f7979cd2e51f5119568275c88ee6f
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2020-03-27 20:53:55 +0900

    implement pure functional versions of qs

--- src/main/scala/mutable/STMonad.scala
+++ src/main/scala/mutable/STMonad.scala
@@ -27,6 +27,23 @@ object ST {
     st[Unit].run(())._1
 }
 
+sealed trait STRef[S,A] {
+  protected var cell: A
+  def read: ST[S,A] = ST(cell)
+  def write(a: => A): ST[S,Unit] = new ST[S,Unit] {
+    def run(s: S) = {
+      cell = a
+      ((), s)
+    }
+  }
+}
+
+object STRef {
+  def apply[S,A](a: A): ST[S, STRef[S,A]] = ST(new STRef[S,A] {
+    var cell = a
+  })
+}
+
 trait RunnableST[A] {
   def apply[S]: ST[S,A]
 }
@@ -35,7 +52,7 @@ sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
   protected def value: Array[A]
   def size: ST[S,Int] = ST(value.size)
 
-  // Write a value at the give index of the array
+  // Write a value at the given index of the array
   def write(i: Int, a: A): ST[S,Unit] = new ST[S,Unit] {
     def run(s: S) = {
       value(i) = a
@@ -48,4 +65,65 @@ sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
 
   // Turn the array into an immutable list
   def freeze: ST[S,List[A]] = ST(value.toList)
+
+  def fill(xs: Map[Int, A]): ST[S,Unit] =
+    xs.foldRight(ST[S,Unit](())) {
+      case ((i, v), st) => st.flatMap(_ => write(i, v))
+    }
+
+  def swap(i: Int, j: Int): ST[S,Unit] = for {
+    x <- read(i)
+    y <- read(j)
+    _ <- write(i, y)
+    _ <- write(j, x)
+  } yield ()
+}
+
+object STArray {
+  def apply[S,A:Manifest](sz: Int, v: A): ST[S, STArray[S,A]] =
+    ST(new STArray[S,A] {
+      lazy val value = Array.fill(sz)(v)
+    })
+
+  def fromList[S,A:Manifest](xs: List[A]): ST[S, STArray[S,A]] =
+    ST(new STArray[S,A] {
+      lazy val value = xs.toArray
+    })
+}
+
+object Immutable {
+  def noop[S] = ST[S,Unit](())
+
+  def partition[S](a: STArray[S,Int], l: Int, r: Int, pivot: Int): ST[S,Int] = for {
+    pivotVal <- a.read(pivot)
+    _ <- a.swap(pivot, r)
+    j <- STRef(l)
+    _ <- (l until r).foldLeft(noop[S])((s, i) => for {
+      _ <- s
+      vi <- a.read(i)
+      _  <- if (vi < pivotVal) (for {
+        vj <- j.read
+        _  <- a.swap(i, vj)
+        _  <- j.write(vj + 1)
+      } yield ()) else noop[S]
+    } yield())
+    x <- j.read
+    _ <- a.swap(x, r)
+  } yield(x)
+
+  def qs[S](a: STArray[S,Int], l: Int, r: Int): ST[S, Unit] = if (l < r) (for {
+    pi <- partition(a, l, r, l + (l-r)/2)
+    _ <- qs(a, l, pi-1)
+    _ <- qs(a, pi+1, r)
+  } yield()) else noop[S]
+
+  def quicksort(xs: List[Int]): List[Int] =
+    if (xs.isEmpty) xs else ST.runST(new RunnableST[List[Int]] {
+      def apply[S] = for {
+        arr    <- STArray.fromList(xs)
+        size   <- arr.size
+        _      <- qs(arr, 0, size - 1)
+        sorted <- arr.freeze
+      } yield sorted
+  })
 }
